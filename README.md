cbb047239c1e8b97b4b99f041e45cfad03d0957b
vishal-mourya <vishal.maurya2050@gmail.com>
2021-03-27 14:32:15 +0530

    Updates and bug fix

---
+++ README.md
@@ -0,0 +1,27 @@
+# Flash Chat - A Lightning Fast Realtime Chatting App Made With Flutter + Firebase ( as backend service )
+
+### About :
+
+- A Simple App which allows any user to register them and get authorized to chat with others present on the server.
+
+- One can learn how to use FireBase as backend,User Defined Widgets,Efficiency of code, async and await and many more stuffs in this flutter App
+<br> <br>
+
+![s1](https://github.com/vishal-mourya/alumni-student-connect/blob/main/pics/s1.png?raw=true) <br> <br>
+
+![s2](https://github.com/vishal-mourya/alumni-student-connect/blob/main/pics/s2.png?raw=true) <br> <br>
+![s3](https://github.com/vishal-mourya/alumni-student-connect/blob/main/pics/s3.png?raw=true) <br> <br>
+![s4](https://github.com/vishal-mourya/alumni-student-connect/blob/main/pics/s4.png?raw=true) <br> <br>
+![s5](https://github.com/vishal-mourya/alumni-student-connect/blob/main/pics/s5.png?raw=true) <br> <br>!
+<br>
+
+- Still Working on Improvements
+
+## Improving This Project
+
+If you feel like I am missing out steps, feel free to make a pull request and contribute to this project. [Github Repo](https://github.com/vishal-mourya/climatge-app)
+
+Would Love to Collaborate with anyone who wishes :)
+
+Thanks & Regards <br>
+[Vishal Mourya](https://www.linkedin.com/in/vishal-mourya-a4245b18b/)
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,163 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/widgets/message_bubble.dart';
+import 'package:flutter/material.dart';
+
+final _firestore = FirebaseFirestore.instance;
+User loggedInUser;
+
+class ChatScreen extends StatefulWidget {
+  static const String id = 'chat_screen';
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser;
+
+      if (user != null) {
+        //  we do have a currently signed in user
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // void getMessages() async {
+  //   //QuerySnapshot is a datatype that comes from firebase
+  //   //which is basically a snapshot of our data stored in firebase
+  //   final messages = await _firestore.collection('messages').get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  // This method will listen the stream of messages
+  // that come from firebase
+  // void messagesStream() async {
+  //   await for (var snapshot in _firestore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                //Implementing logout functionality
+                _auth.signOut();
+                Navigator.pop(context);
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MessagesStream(),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      messageTextController.clear();
+                      _firestore.collection('messages').add({
+                        'sender': loggedInUser.email,
+                        'text': messageText,
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data.docs.reversed;
+        List<MessageBubble> messageBubbles = [];
+
+        for (var message in messages) {
+          final messageSender = message.data().values.elementAt(0);
+          final messageText = message.data().values.elementAt(1);
+
+          final currenUser = loggedInUser.email;
+
+          final messageBubble = MessageBubble(
+            sender: messageSender,
+            text: messageText,
+            isMe: currenUser == messageSender,
+          );
+          messageBubbles.add(messageBubble);
+        }
+
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            children: messageBubbles,
+          ),
+        );
+
+        return null;
+      },
+    );
+  }
+}
